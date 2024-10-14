01d9fb709a9b026f3f6f8dff45034dc0641bf2a2
Rishu Raj <rishuraj17jan@gmail.com>
2024-10-14 11:43:17 +0530

    learning php

---
+++ resources/views/register.blade.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+
+    </head>
+
+    <body>
+        <div sttyle="padding-left:200px">
+            <h5>Register</h5>
+        @if($errors->any())
+        <ul>
+            
+        </ul>
+        </div>
+    </body>
+</html>
