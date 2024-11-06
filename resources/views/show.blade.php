07cb2b7abceb1f2fb39bced3e60802d2fd73c5d4
Rishu Raj <rishuraj17jan@gmail.com>
2024-11-06 10:20:34 +0530

    class assesment

---
+++ resources/views/show.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Profile</title>
+</head>
+<body>
+    <h1>Show Profile</h1>
+
+    <p><strong>User ID:</strong>{{$userId}}</p>
+
+
+    <img src="{{asset('img2.png')}}" alt="Profile Image" width="150" height="150">
+
+    
+</body>
+</html>
